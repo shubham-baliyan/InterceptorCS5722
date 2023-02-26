62d3510c13fe768bad8a5b7c8be53cc8b74eaffa
shubham baliyan <shubham.baliyan204@gmail.com>
2023-02-26 22:39:06 +0000

    added the Context object

--- Context.java
+++ Context.java
@@ -2,8 +2,34 @@ package headfirst.designpatterns.observer.weather;
 
 public class Context
 {
-    public void doSomething(){
-        System.out.println("LOL");
+   public Context(float temperature,float humidity,float pressure, WeatherData weatherData){
+        this.humidity=humidity;
+        this.temperature=temperature;
+        this.pressure=pressure;
+        this.weatherData =weatherData;
+
+    }
+    private WeatherData weatherData;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private void doSomething(){
+        System.out.println("Hi the temperatur and stuff is unsusal");
     }
 
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public WeatherData getWeatherData() {
+        return weatherData;
+    }
 }
