e90b4f0c54a6e385a167df2a5a2a68eb94daa072
shubham baliyan <shubham.baliyan204@gmail.com>
2023-02-26 22:40:27 +0000

    added the TemperatureConverterInterceptor

--- TemperatureConverterInterceptor.java
+++ TemperatureConverterInterceptor.java
@@ -3,7 +3,9 @@ package headfirst.designpatterns.observer.weather;
 public class TemperatureConverterInterceptor implements  Interceptor{
     @Override
     public boolean onPreMarshallRequest(Context context, String arg) {
-        return false;
+        float newTemp = (context.getTemperature()-32) *5/9;
+        System.out.println("The converted temp from fahrenheit to celsius ="+newTemp);
+        return true;
     }
 
     @Override
